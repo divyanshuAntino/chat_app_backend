6d0ec8f542a0759fe6a418b34422fe522fac8eb4
divyanshu050303 <divyanshusingh6747@gmail.com>
2025-04-13 20:35:30 +0530

    login api

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/divyanshu050303/chat-app-backend/helper"
 	"github.com/divyanshu050303/chat-app-backend/models"
@@ -58,5 +59,45 @@ func (ctrl *UserController) Createuser(c *fiber.Ctx) error {
 	return nil
 }
 func (ctrl *UserController) LoginUser(c *fiber.Ctx) error {
+	userModel := models.UserModels{}
+	err := c.BodyParser(&userModel)
+	if err != nil {
+		helper.ApiResponse(c, http.StatusBadRequest, "Bad request", nil)
+		return err
+	}
+	var existingUser models.UserModels
+	err = ctrl.Repo.DB.Where("user_email=?", userModel.UserEmail).Find(&existingUser).Error
+	if err != nil {
+		helper.ApiResponse(c, http.StatusBadRequest, "Bad Request", nil)
+		return err
+	}
+
+	if existingUser.UserId == "" {
+		helper.ApiResponse(c, http.StatusNotFound, "User Not Found with this email id", nil)
+		return err
+	}
+	if !strings.EqualFold(*existingUser.UserPassword, *userModel.UserPassword) {
+		helper.ApiResponse(c, http.StatusUnauthorized, "Invalid Password", nil)
+		return nil
+
+	}
+	accessToken, refreshToken, err := helper.GenerateToken(userModel)
+	if err != nil {
+		helper.ApiResponse(c, http.StatusBadRequest, "Could not generate the accesstoken", nil)
+		return err
+	}
+	data := map[string]any{
+		"token": map[string]string{
+			"accessToken":  accessToken,
+			"refreshToken": refreshToken,
+		},
+		"user": map[string]any{
+			"id":        existingUser.UserId,
+			"userName":  existingUser.UserName,
+			"userEmail": existingUser.UserEmail,
+		},
+	}
+	helper.ApiResponse(c, http.StatusOK, "Login successfully", data)
+
 	return nil
 }
